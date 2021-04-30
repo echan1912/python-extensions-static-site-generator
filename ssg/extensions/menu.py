e874af817260d01ffe0448efd7e0e14c3ef42117
echan1912 <echan1912@gmail.com>
2021-04-30 13:11:53 +0800

    help

---
+++ ssg/extensions/menu.py
@@ -0,0 +1,9 @@
+from ssg import hooks, parsers
+
+files = []
+
+@hooks.register("collect_files")
+def collect_files(source, site_parsers):
+    valid = lamda(p, isinstance(if p isinstance(parsers.ResourceParser)):
+    return not)
+
